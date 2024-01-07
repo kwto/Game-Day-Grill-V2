13c7d8455ed0036866545e8933452a19066d5409
Kathir Dev <121499256+kwto@users.noreply.github.com>
2024-01-07 17:36:18 -0600

    stuff so far
    did some things

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,11 +1,30 @@
+import Image from "next/image";
+import logo from "@/src/images/logo.png";
 import React from "react";
 
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul className="navbar flex justify-between items-center">
+        <li><Image src={logo} alt="Logo" className="logo" width={150} height={50}/></li>
+        <div className="flex space-x-4">
+          <li><a href="">HOME</a></li>
+          <li><a href="contact">ABOUT US</a></li>
+          <li><a href="menu">MENU</a></li>
+          <li><a href="checkout">CATERING</a></li>
+        </div>
+      </ul>
+    </nav>
+  );
+}
+
+
+
 export default function Home() {
-    return (
-        <main>
-            <div className="bg-white">
-                <h1 className="text-black">snigger</h1>
-            </div>
-        </main>
-    );
+  return (
+    <div>
+      <NavBar />
+    </div>
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,16 @@
-import React from 'react'
-import Image from 'next/image'
-import './globals.css'
-import logo from "@/src/images/logo.png"
-import backgroundImage from "@/src/images/EAT.png"
-import mainimage from "@/src/images/mainpage.png"
+// App.tsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import Home from "@/app/home/page";
 
-
-const NavBar = () => {
+const App: React.FC = () => {
   return (
-    <nav>
-      <ul className="navbar flex justify-between items-center">
-        <li><Image src={logo} alt="Logo" className="logo" width={150} height={50}/></li>
-        <div className="flex space-x-4">
-          <li><a href="">HOME</a></li>
-          <li><a href="contact">ABOUT US</a></li>
-          <li><a href="menu">MENU</a></li>
-          <li><a href="checkout">CATERING</a></li>
-        </div>
-      </ul>
-    </nav>
+    <Router>
+      <Route path="/" exact component={Home} />
+      {/* Add more routes if needed */}
+      <Redirect to={{ pathname: 'http://localhost:3000/home' }} />
+    </Router>
   );
-}
+};
 
-const Footer = () => {
-  return (
-    <footer className="fixed bottom-0 w-full">
-      <ul className="navbar flex justify-between items-center p-4 bg-white">
-        <li>
-          <a href="/">
-            <Image src={logo} alt="Logo" className="logo" width={150} height={50} />
-          </a>
-        </li>
-      </ul>
-    </footer>
-  );
-}
-
-export default function Home() {
-  return (
-    <div>
-      <NavBar />
-      <Image src={backgroundImage} alt="Background" layout="responsive" width={1920} height={1080}/>
-      <main>
-      <Image src={mainimage} alt="Main" layout="responsive" width={1920} height={1080}/>
-      </main>
-      <Footer />
-    </div>
-  )
-}
+export default App;
