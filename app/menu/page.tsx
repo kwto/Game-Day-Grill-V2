0e1e8fdca8945263ddf1766024de8a84c2195fab
Kathir Dev <121499256+kwto@users.noreply.github.com>
2024-01-07 17:58:11 -0600

    so far
    far

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -9,7 +9,7 @@ const NavBar = () => {
       <ul className="navbar flex justify-between items-center">
         <li><Image src={logo} alt="Logo" className="logo" width={150} height={50}/></li>
         <div className="flex space-x-4">
-          <li><a href="">HOME</a></li>
+          <li><a href="/">HOME</a></li>
           <li><a href="contact">ABOUT US</a></li>
           <li><a href="menu">MENU</a></li>
           <li><a href="checkout">CATERING</a></li>
