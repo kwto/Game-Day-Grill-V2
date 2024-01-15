d6c178d8da03fd0a36e2bbec489dc59395e5f6fc
Kathir Dev <121499256+kwto@users.noreply.github.com>
2024-01-15 16:25:05 -0600

    this kinda work but error
    idk how error

---
+++ src/comps/ShoppingCart.tsx
@@ -0,0 +1,36 @@
+import { Offcanvas, Stack } from "react-bootstrap"
+import { useShoppingCart } from "@/src/context/ShoppingCartContext"
+import { formatCurrency } from "@/src/util/formatCurrency"
+import { CartItem } from "@/src/comps/CartItem"
+import storeItems from "@/src/store/items.json"
+
+type ShoppingCartProps = {
+  isOpen: boolean
+}
+
+export function ShoppingCart({ isOpen }: ShoppingCartProps) {
+  const { closeCart, cartItems } = useShoppingCart()
+  return (
+    <Offcanvas show={isOpen} onHide={closeCart} placement="end">
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title>Cart</Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body>
+        <Stack gap={3}>
+          {cartItems.map(item => (
+            <CartItem key={item.id} {...item} />
+          ))}
+          <div className="ms-auto fw-bold fs-5">
+            Total{" "}
+            {formatCurrency(
+              cartItems.reduce((total, cartItem) => {
+                const item = storeItems.find(i => i.id === cartItem.id)
+                return total + (item?.price || 0) * cartItem.quantity
+              }, 0)
+            )}
+          </div>
+        </Stack>
+      </Offcanvas.Body>
+    </Offcanvas>
+  )
+}
--- src/comps/StoreItems.tsx
+++ src/comps/StoreItems.tsx
@@ -1,16 +1,25 @@
-import React from "react";
-import { Card } from "react-bootstrap";
+'use client'
+import { Button, Card } from "react-bootstrap"
+import { useShoppingCart } from "@/src/context/ShoppingCartContext"
+import { formatCurrency } from "@/src/util/formatCurrency"
 
 type StoreItemProps = {
-  id: number,
-  name: string
-  price: number
-  imgUrl: string
+    id: number
+    name: string
+    price: number
+    imgUrl: string
 }
 
+export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
+    const {
+        getItemQuantity,
+        increaseCartQuantity,
+        decreaseCartQuantity,
+        removeFromCart,
+    } = useShoppingCart()
+    
+    const quantity = getItemQuantity(id)
 
-
-export function StoreItem ({ id, name, price ,imgUrl}: StoreItemProps ) {
     return (
         <Card className="h-100">
             <Card.Img
@@ -22,8 +31,38 @@ export function StoreItem ({ id, name, price ,imgUrl}: StoreItemProps ) {
             <Card.Body className="d-flex flex-column">
                 <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
                     <span className="fs-2">{name}</span>
-                    <span className="ms-2 text-muted margin-left=100px">{(price)}</span>
+                    <span className="ms-2 text-muted">{formatCurrency(price)}</span>
                 </Card.Title>
+                <div className="mt-auto">
+                    {quantity === 0 ? (
+                        <Button className="w-100" onClick={() => increaseCartQuantity(id)}>
+                            + Add To Cart
+                        </Button>
+                    ) : (
+                        <div
+                            className="d-flex align-items-center flex-column"
+                            style={{ gap: ".5rem" }}
+                        >
+                            <div
+                                className="d-flex align-items-center justify-content-center"
+                                style={{ gap: ".5rem" }}
+                            >
+                                <Button onClick={() => decreaseCartQuantity(id)}>-</Button>
+                                <div>
+                                    <span className="fs-3">{quantity}</span> in cart
+                                </div>
+                                <Button onClick={() => increaseCartQuantity(id)}>+</Button>
+                            </div>
+                            <Button
+                                onClick={() => removeFromCart(id)}
+                                variant="danger"
+                                size="sm"
+                            >
+                                Remove
+                            </Button>
+                        </div>
+                    )}
+                </div>
             </Card.Body>
         </Card>
     )
