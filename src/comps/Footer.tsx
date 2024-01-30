78017bce7470b5718e2be07a7b61aaa8f14884ba
Kathir Dev <121499256+kwto@users.noreply.github.com>
2024-01-29 19:45:16 -0600

    footer/navbar
    added daisyui to navbar and footer

--- src/comps/Footer.tsx
+++ src/comps/Footer.tsx
@@ -1,17 +1,35 @@
-import Image from "next/image";
-import logo from "@/public/imgs/logo.png";
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
 export const Footer = () => {
+
+    const animations = ["animate-spin", "animate-ping", "animate-bounce", "animate-pulse"];
+    const [randomElement, setRandomElement] = useState('');
+
+    const handleClick = () => {
+        let newRandomElement;
+        do {
+            newRandomElement = animations[Math.floor(Math.random() * animations.length)];
+        } while (newRandomElement === randomElement);
+        console.log(newRandomElement);
+        setRandomElement(newRandomElement);
+    }
+
+    // Run handleClick once on component mount
+    useEffect(handleClick, []);
+
     return (
-        <footer >
-            <ul className="navbar flex justify-between items-center p-4 bg-white">
-                <li>
-                    <a href="/">
-                        <Image src={logo} alt="Logo" className="logo" width={150} height={50} />
-                    </a>
-                </li>
-            </ul>
-        </footer>
+        <div className="navbar bg-base-100">
+            <div className="flex-1">
+                <img alt="logo"
+                     src="/imgs/logo.png"
+                     className="logo" width={50} height={50}
+                />
+            </div>
+            <button
+                onClick={handleClick}
+                className={`btn text-xl align-middle hover:${randomElement}`}
+            >   Game Day Grill
+            </button>
+        </div>
     );
 }
