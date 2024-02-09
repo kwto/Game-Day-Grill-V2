8e05eb97257589e87efbfe1929a4f6ce41a8db00
Kathir Dev <121499256+kwto@users.noreply.github.com>
2024-02-09 12:55:52 -0600

    not needed right now
    current

--- src/comps/StoreItems.tsx
+++ src/comps/StoreItems.tsx
@@ -1,30 +1,29 @@
-import React from "react";
-import { Card } from "react-bootstrap";
+// import React from "react";
 
-type StoreItemProps = {
-  id: number,
-  name: string
-  price: number
-  imgUrl: string
-}
-
+// type StoreItemProps = {
+//   id: number,
+//   name: string
+//   price: number
+//   imgUrl: string
+// }
 
 
-export function StoreItem ({ id, name, price ,imgUrl}: StoreItemProps ) {
-    return (
-        <Card className="h-100">
-            <Card.Img
-                variant="top"
-                src={imgUrl}
-                height="200px"
-                style={{ objectFit: "cover" }}
-            />
-            <Card.Body className="d-flex flex-column">
-                <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
-                    <span className="fs-2">{name}</span>
-                    <span className="ms-2 text-muted margin-left=100px">{(price)}</span>
-                </Card.Title>
-            </Card.Body>
-        </Card>
-    )
-}
+
+// export function StoreItem ({ id, name, price ,imgUrl}: StoreItemProps ) {
+//     return (
+//         <Card className="h-100">
+//             <Card.Img
+//                 variant="top"
+//                 src={imgUrl}
+//                 height="200px"
+//                 style={{ objectFit: "cover" }}
+//             />
+//             <Card.Body className="d-flex flex-column">
+//                 <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+//                     <span className="fs-2">{name}</span>
+//                     <span className="ms-2 text-muted margin-left=100px">{(price)}</span>
+//                 </Card.Title>
+//             </Card.Body>
+//         </Card>
+//     )
+// }
