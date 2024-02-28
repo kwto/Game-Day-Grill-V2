1de89c8d136caaacc15fdfb4c29619314196e261
Kathir Dev <121499256+kwto@users.noreply.github.com>
2024-02-28 03:45:51 -0600

    so many errors
    help

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -45,12 +45,7 @@ export default function Home() {
             </div>
             <main className=" min-h-screen">
 
-                <div className="max-w-5xl mx-auto py-8">
-                    <div className="flex items-center justify-between border-b pb-3">
-                        <h1 className="font-semibold tracking-wide leading-10 text-xl lg:text-3xl">
-                            Shop Now
-                        </h1>
-                    </div>
+                
 
                     <div className="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 xl:gap-x-8">
                         {
@@ -59,7 +54,6 @@ export default function Home() {
                             ))
                         }
                     </div>
-                </div>
             </main>
 
             <Footer/>
--- src/comps/PricingCard.jsx
+++ src/comps/PricingCard.jsx
@@ -1,42 +1,73 @@
-import axios from "axios"
-import Link from "next/link"
-import {AiFillCheckCircle} from 'react-icons/ai'
-import {getPriceTotal, getProductImage, getProductName} from "@/src/comps/computed"
+import axios from "axios";
+import {getProductName, getPriceTotal, getProductImage, getProductDescription} from "@/src/comps/items";
 
-export default function Home({price}) {
+export default function Home({ price }) {
 
+    const handleSubscription = async (e) => {
+        e.preventDefault();
+        const { data } = await axios.post('/api/payment',
+        {
+          priceId: price.id
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+        );
+        window.location.assign(data)
+      }
+    
+
+      const imageNames = [
+        'ChipsandQueso.png',
+        'MozzarellaSticks.png',
+        'GamedayWings.png',
+        'FriedCalamari.png',
+        'GamedayBurger.png',
+        'ChickenTenders.png',
+        'ChickenSandwich.png',
+        'GrilledChickenSandwich.png',
+        'Veganburger.png',
+        'Theclassic.png',
+        'Icecreamsundae.png',
+        'cheesecake.png',
+        'tiramisu.png',
+        'applepie.png',
+        'Cocacola.png',
+        'StrawberrySmoothies.png',
+        'ChocolateShake.png',
+        'OrangeJuice.png'
+      ];
 
     return (
         <div>
-            <div className="relative">
-                <div className=" w-full h-72 rounded-lg">
-                    <img
-                        src={getProductImage(price.product)}
-                        className="w-full h-full object-center object-cover"
-                    />
+            {price.product && (
+                <div className="relative">
+                    {/* <div className=" w-full h-72 rounded-lg">
+                        <image
+                            src={getProductImage(price.product)}
+                            alt={getProductDescription(price.product)}
+                            className="w-full h-full object-center object-cover"
+                        />
+                    </div> */}
+                    <div className="relative mt-4">
+                        <h3 className="text-sm font-medium ">{getProductName(price.product)}</h3>
+                    </div>
+                    <div className="absolute top-0 inset-x-0 h-full rounded-lg p-4 flex items-end justify-end overflow-hidden">
+                        <div aria-hidden="true" className="absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50" />
+                        <h4 className="text-lg font-bold text-white">
+  {price.nickname}
+  {imageNames.map((name, index) => (
+    <image key={index} src={`../images/${name}`} alt="Product" />
+  ))}
+</h4>
+                        <btn className="btn ml-5 mr-4 mb-4" onClick={handleSubscription}>
+                             Add To Cart {price.metadata.price} -  ${getPriceTotal(price)}
+                        </btn>
+                    </div>
                 </div>
-                <div className="relative mt-4">
-                    <h3 className="text-sm font-medium ">{getProductName(price.product)}</h3>
-                </div>
-                <div
-                    className="absolute top-0 inset-x-0 h-72 rounded-lg p-4 flex items-end justify-end overflow-hidden">
-                    <div
-                        aria-hidden="true"
-                        className="absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50"
-                    />
-                    <p className="relative text-lg font-semibold ">
-                        ${getPriceTotal(price)}
-                    </p>
-                </div>
-            </div>
-            <div className="mt-6">
-                {/*<button*/}
-                {/*    onClick={() => addToCart(price)}*/}
-                {/*    className="relative w-full flex bg-gray-200 border border-transparent rounded-md py-2 px-8 items-center justify-center text-sm font-medium text-gray-900 hover:bg-gray-200"*/}
-                {/*>*/}
-                {/*    Add to bag<span className="sr-only">, {getProductName(price.product)}</span>*/}
-                {/*</button>*/}
-            </div>
+            )}
         </div>
-    )
+    );
 }
